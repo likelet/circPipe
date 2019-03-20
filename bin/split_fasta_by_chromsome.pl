759fb64151aa8512fc66ee1f698b2c67d98710d3
likelet <zhaoqi699@sina.com>
2019-03-20 20:10:47 +0800

    fix the Perlscrit

--- bin/split_fasta_by_chromsome.pl
+++ bin/split_fasta_by_chromsome.pl
@@ -1,20 +1,21 @@
 #!/usr/bin/perl
-#usage: perl split_fasta_by_chrom.pl <genome.fa>
+#usage: perl split_fasta_by_chrom.pl <genome.fa> <outDIR>
 $f = $ARGV[0]; #get the file name
+$o = $ARGV[1]; #get the DIR name with "/"
 
-open (INFILE, "<$f")
-or die "Can't open: $f $!";
+open (INFILE, "<$f") or die "Can't open: $f $!";
 
 while (<INFILE>) {
 $line = $_;
 chomp $line;
 if ($line =~ /\>/) { #if has fasta >
-close OUTFILE;
-$new_file = split "\s", substr($line,1) ;
-$new_file .= ".fa";
-open (OUTFILE, ">$new_file")
-or die "Can't open: $new_file $!";
+	#split (/\n/,$_)
+	@tmp = split (/\s/, substr($line,1)) ;
+	$new_file = $tmp[0];
+	#print $new_file."\n";
+	$new_file .= ".fa";
+	open (OUTFILE, ">$o/$new_file") or die "Can't open: $new_file $!";
+	}
+	print OUTFILE "$line\n";
 }
-print OUTFILE "$line\n";
-}
-close OUTFILE;
+	close OUTFILE;
